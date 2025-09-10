cb00ab9babf04a82416b1235a3d7af1c0bba9c02
Cheikhfall03 <cheikh.fall@aims-senegal.org>
2025-09-10 15:11:58 +0000

    Update router_query.py

--- chains/router_query.py
+++ chains/router_query.py
@@ -20,7 +20,7 @@ class RouteQuery(BaseModel):
 # LLM with function call
 
 llm = ChatGroq(
-    model="gemma2-9b-it",
+    model="llama-3.1-8b-instant",
     temperature=0.0,
     api_key=os.getenv("GROQ_API_KEY")
 )
